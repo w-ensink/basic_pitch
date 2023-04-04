9244d0f6e930667493727a7fa1509e6434e130bf
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2023-04-04 22:41:03 +0200

    first attempt at android CI build

--- build-android.sh
+++ build-android.sh
@@ -1,4 +1,4 @@
-export ANDROID_NDK_HOME=$HOME/Library/Android/sdk/ndk/25.2.9519653
+# export ANDROID_NDK_HOME=$HOME/Library/Android/sdk/ndk/25.2.9519653
 export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android33-clang++
 
 cargo rustc --lib --target=aarch64-linux-android -- \
